0a64a8d78246a9be5210144ddda03875e4342862
Lyhq1996 <lihongqian_shufe@163.com>
2022-09-09 13:12:53 -0400

    add react context api for check out page

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="header">
-            <img
-                className="header_logo"
-                src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
-            />
+            <Link to="/">
+                <img
+                    className="header_logo"
+                    src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                    alt=""
+                />
+            </Link>
 
             <div className="header_search">
                 <input
@@ -34,16 +41,15 @@ function Header() {
                     <span className="header_optionLineOne">Your</span>
                     <span className="header_optionLineTwo">Prime</span>
                 </div>
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header_optionLineTwo header_basketCount">0</span>
-
-                </div>
-
-
-
+                <Link to="/checkout">
+                    <div className="header_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header_optionLineTwo header_basketCount">
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
             </div>
-
         </div>
     )
 }
