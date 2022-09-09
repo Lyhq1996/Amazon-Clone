0452e30e58c671f342211b5fc63e043694e13678
Lyhq1996 <lihongqian_shufe@163.com>
2022-09-09 16:05:56 -0400

    built front end of checkout and login page

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
+
         <Routes>
+
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>}>
+          </Route>
+
           <Route
             path="/checkout"
-            element={<>
-
-              <Checkout />
-            </>}>
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>}>
           </Route>
+
           <Route
             path="/"
-            element={<>
-              <Home />
-            </>}>
+            element={
+              <>
+                <Header />
+                <Home />
+              </>}>
           </Route>
+
         </Routes>
 
       </div>
